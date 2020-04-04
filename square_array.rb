b0d332a9c8abd6ff8fa21608a1e3a534e708ce6c
heyserenadinh Dinh <heyserenadinh@gmail.com>
2020-04-04 20:23:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   square_array = []
   array.each do |num|
     square = num ** 2
-    squared_array << square
+    square_array << square
 end
-squared_array
+square_array
 end
